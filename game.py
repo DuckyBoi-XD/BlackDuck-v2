bc9faf405297b14c713f0273a1c9def1544a9f2c
Braeden Sy Tan <braedenjairsytan@icloud.com>
2026-07-19 00:01:35 +1200

    WORKING, game continuation without payout

--- game.py
+++ game.py
@@ -894,28 +894,49 @@ class game_functions:
                         GV.dStart = True
                         GV.dOutcome = False
 
+                        GV.cardPositions1 = []
+                        GV.cardPositions2 = []
+                        GV.cardPositions3 = []
+                        GV.cardPositions4 = []
+                        GV.cardPositions = [GV.cardPositions1, GV.cardPositions2, GV.cardPositions3, GV.cardPositions4]
+                        GV.dcardPosition = []
+
+                        GV.addCard = [0, 0, 0, 0]
+                        GV.daddCard = 0
+
+                        GV.gameStart = [0, 0, 0, 0]
+                        GV.dTurn = False
+                        GV.dDrawTime = 90
+
                         GV.cardStartPos1 = [120, 240]
                         GV.cardStartPos2 = [400, 300]
                         GV.cardStartPos3 = [780, 300]
                         GV.cardStartPos4 = [1050, 240]
+                        GV.cardStartPos = [GV.cardStartPos1, GV.cardStartPos2, GV.cardStartPos3, GV.cardStartPos4]
                         GV.dcardStartPos = [594, 80]
-                        GV.dDrawTime = 90
-                        GV.dHandValue = 0
-                        GV.daddCard = 0
-                        GV.dTurn = False
-                        
-                        clearlist = [GV.dCardHand, GV.dCardValues, GV.dcardPosition]
-                        clearlists = [GV.cardPositions, GV.CardValues, GV.CardHands, GV.gameChipPos]
-                        clear0lists = [GV.addCard, GV.gameStart, GV.gameBet, GV.gameCHIPS1, GV.gameCHIPS2, GV.HandValues, GV.gamefocus, GV.gameOutput, GV.HandState]
 
-                        for list_var in clearlist:
-                            list_var.clear()
-                        for list_var in clearlists:
-                            for value in list_var:
-                                value.clear()
-                        for list_0 in clear0lists:
-                            for value in list_0:
-                                value = 0
+                        GV.CardHand1 = []
+                        GV.CardHand2 = []
+                        GV.CardHand3 = []
+                        GV.CardHand4 = []
+                        GV.CardHands = [GV.CardHand1, GV.CardHand2, GV.CardHand3, GV.CardHand4]
+                        GV.dCardHand = []
+
+                        GV.HandValues = [0, 0, 0, 0]
+                        GV.dHandValue = 0
+
+                        GV.CardValues1 = []
+                        GV.CardValues2 = []
+                        GV.CardValues3 = []
+                        GV.CardValues4 = []
+                        GV.CardValues = [GV.CardValues1, GV.CardValues2, GV.CardValues3, GV.CardValues4]
+                        GV.dCardValues = []
+
+                        GV.dbust = False
+                        GV.gamefocus = [0, 0, 0, 0]
+                        GV.HandState = [0, 0, 0, 0] # 1 stand, 2 bust, 3 blackjack, 4 five card charlie
+                        GV.gameOutput = [0, 0, 0, 0] # 1 tie, 2 lose, 3, win
+                        GV.dOutcome = False
 
                         for indexs, value in enumerate(GV.chipBet):
                             if len(value) != 0:
